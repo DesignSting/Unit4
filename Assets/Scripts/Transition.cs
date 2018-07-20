72e3bf5a49002e7c59a14d66cdf5560c922ce49a
DesignSting <39390336+DesignSting@users.noreply.github.com>
2018-07-20 13:24:40 +1000

    Revision 8: Few changes, few additions

---
+++ Assets/Scripts/Transition.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Transition : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("I AM FUCKING HERE!!!");
+        SceneManager.LoadScene(1);
+        if (other.tag == "Player")
+        {
+            
+        }
+    }
+}
